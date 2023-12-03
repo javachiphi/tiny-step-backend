1862103cdc99c3b2481ec8dcf883090d531a6fc6
Sophie Y <javachiphi@gmail.com>
2023-12-03 20:50:27 +0800

    CRUD operations to tag

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -2,25 +2,72 @@ const BaseController = require("./baseController");
 
 
 class tagController extends BaseController {
-    constructor(){
-        super(); 
+    constructor(model){
+        super(model); 
     }
 
-    getOne(req, res){
-        res.send('get single tag');
+    async getOne(req, res){
+        try {
+            const { tagId } = req.params
+            const found = await this.model.findByPk(tagId);
+            res.send(found)
+        } catch(error) {
+            console.log('error', error);
+            res.status(500).send('Error getting one')
+        }
     }
 
-    createOne(req, res){
-        res.send('create single tag');
+    async createOne(req, res){
+        try {
+            console.log('req.body', req.body)
+            const { note, description, type, personality } = req.body;
+            const newTag = await this.model.create({
+                note: note,
+                description: description || null,
+                type: type || null,
+                personality: personality || null
+            })
+            res.send(newTag);
+        } catch(error) {
+            console.log('error', error);
+            res.status(500).send('Error creating tag')
+        }
+
     }
 
 
-   updateOne(req, res){
-        res.send('update tag');
+   async updateOne(req, res){
+        try {
+            console.log('req.body', req.body);
+            const { tagId } = req.params; 
+            const found = await this.model.findByPk(tagId);
+
+            const { note, description, type, personality } = req.body;
+
+            found.note = note;
+            found.description = description;
+            found.type = type;
+            found.personality = personality 
+
+            const updated = await found.save();
+
+            res.send(updated);
+        } catch(error) {
+            console.log('error', error);
+            res.status(500).send('Error updating')
+        }
     }
 
-    deleteOne(req, res){
-        res.send('delete tag');
+    async deleteOne(req, res){
+        try {
+            const { tagId } = req.params; 
+            const found = await this.model.findByPk(tagId);
+            found.destroy()
+            res.send("deleted");
+        } catch(error) {
+            console.log('error', error);
+            res.status(500).send('Error deleting')
+        }
     }
 }
 
