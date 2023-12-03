fda6c35476a02acef1526298e1738f62d0966163
Sophie Y <javachiphi@gmail.com>
2023-12-03 23:47:05 +0800

    pass params to tag controller

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const db = require('./db/models/index')
-const { entry, tag } = db;
+const { entry, tag, user } = db;
+
 
 
 const EntryRouter = require('./routers/entryRouter.js')
@@ -13,11 +16,11 @@ const TagController = require('./controllers/tagController.js')
 
 
 
-const entryController = new EntryController(entry)
+const entryController = new EntryController(entry, user)
 const entryRouter = new EntryRouter(entryController).routes()
-app.use('/entries', entryRouter);
+app.use('/users/:userId/entries', entryRouter);
 
-const tagController = new TagController
+const tagController = new TagController(tag, user, entry)
 const tagRouter = new TagRouter(tagController).routes()
 app.use('/tags', tagRouter);
 
