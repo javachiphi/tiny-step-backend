6729c5592a54d7e6a4a75e3fbb1d929eaeb1ed96
Sophie Y <javachiphi@gmail.com>
2023-12-30 12:14:41 +0800

    apply default seeder upon new user creation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,59 +1,35 @@
 const BaseController = require("./baseController");
+const defaultSeeder = require('../utils/defaultSeeder');
 
 
 class userController extends BaseController {
-    constructor(model){
+    constructor(model, entryModel, tagModel){
         super(model); 
+        this.defaultSeeder = new defaultSeeder(model, entryModel, tagModel);
     }
-    async createOne(req, res){
+    
+    async createOne(req, res) {
         try {
-
             const jwtSub = req.auth.payload.sub;
-
-            const user = await this.model.findOrCreate({
+            const [user, created] = await this.model.findOrCreate({
                 where: { jwtSub: jwtSub },
-                defaults: {
-                    jwtSub: jwtSub
-                  }
-              });
+                defaults: { jwtSub: jwtSub }
+            });
+    
+            if (created) {
+                const seed = await this.defaultSeeder.seedDefaultData(jwtSub);
+                res.status(200).send({ user, seed });
+            } else {
 
-            res.status(200).send(user);
+                res.status(200).send(user);
+            }
+    
         } catch(error) {
             console.log('error', error);
-            res.status(500).send('Error creating')
+            res.status(500).send('Error creating');
         }
     }
-
-    async getOne(req, res){
-        try {
-            const { userId } = req.params
-            const found = await this.model.findByPk(userId);
-            res.send(found)
-        } catch(error) {
-            console.log('error', error);
-            res.status(500).send('Error getting')
-        }
-    }
-
-   async updateOne(req, res){
-        try {
-            const { userId } = req.params;
-            const { personality, email, password } = req.body;
-
-            const found = await this.model.findByPk(userId);
-
-            found.personality = personality;
-            found.email = email;
-            found.password = password; 
-
-            const updated = await found.save(); 
-            res.send(updated);
-        } catch(error) {
-            console.log('error', error);
-            res.status(500).send('Error updating')
-        }
-    }
-
+    
     async deleteOne(req, res){
         try {
             const { userId } = req.params; 
